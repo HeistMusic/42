356b0efba7d89f57e5020fcb230bebf6a70bb7bb
Luis Felipe Rodriguez Araque <luis-rod@c3r5s3.42madrid.com>
2024-01-27 14:09:41 +0100

    correccion de errores parte mil, strdup

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: luis-rod <luis-rod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 13:41:36 by luis-rod          #+#    #+#             */
-/*   Updated: 2024/01/21 17:16:01 by luis-rod         ###   ########.fr       */
+/*   Updated: 2024/01/27 14:05:12 by luis-rod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char	*dup;
-	int		i;
+	char		*dup;
+	size_t		i;
 
 	i = 0;
-	dup = (char *) malloc((char)ft_strlen(s1) + 1);
+	dup = malloc((ft_strlen(s1) + 1) * sizeof(char));
 	if (!dup)
 		return (NULL);
 	while (s1[i])
